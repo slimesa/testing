04efec75e8695611467a6996a5e27ddd940c1324
sunshine <isaacneston839@gmail.com>
2023-09-22 01:13:32 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        customP: '#140B5C',
+        customG: '#595858',
+      },
+    },
+  },
+  plugins: [],
+}
+
